7a516121e27ba69c435f9ebf2a39f7296a430289
Goraksh <gorakshpatil001@gmail.com>
2026-03-10 21:53:18 +0530

    Added Controller layer

---
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -0,0 +1,52 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Task;
+import com.example.demo.service.TaskService;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+	@Autowired
+	TaskService crepo;
+	
+	@GetMapping
+	public List<Task>getAllTask(){
+		return crepo.getAllTask();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Task> getById(@PathVariable Long id) {
+		return crepo.getById(id);
+	}
+	
+	@PostMapping
+	public Task createTask(@RequestBody Task s) {
+		return crepo.createTask(s);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteTask(@PathVariable long id) {
+		 crepo.deleteTask(id);
+	}
+	 
+	@PutMapping("/{id}")
+	public Task updateTask(@PathVariable Long id, @RequestBody Task updatedtask) {
+		return crepo.updateTask(id, updatedtask);
+	}
+	
+	
+}
